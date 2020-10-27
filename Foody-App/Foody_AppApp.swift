eb401bf7c5f2a8af2c93c9bb3a10f52dae43ee13
safarsafarov <safarov.safar@hotmail.com>
2020-10-27 17:04:33 +0800

    commit

---
+++ Foody-App/Foody_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foody_AppApp.swift
+//  Foody-App
+//
+//  Created by Safar Safarov on 2020/10/27.
+//
+
+import SwiftUI
+
+@main
+struct Foody_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
