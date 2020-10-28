268d26691c829b4fe967672ee9cf351d4b3e51a2
safarsafarov <safarov.safar@hotmail.com>
2020-10-28 11:05:14 +0800

    commit🪄

---
+++ Foody-App/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Foody-App
+//
+//  Created by Safar Safarov on 2020/10/28.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "line.horizontal.3")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor("pink")
+            }
+        }
+    }
+}
---
+++ Foody-App/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Foody-App
+//
+//  Created by Safar Safarov on 2020/10/28.
+//
+
+import Foundation
